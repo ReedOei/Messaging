daa7fd486278c9801efc502d459640b1b4996f9a
Reed Oei <oei.reed@gmail.com>
2021-03-06 13:13:03 -0600

    Initial

---
+++ client.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "Name: "
+read -r name
+
+(
+    echo "\"account(\\\"$name\\\")\""
+
+    while read -r line; do
+        echo "\"text($(date +"%s"), \\\"$(echo "$line" | sed -E "s/\"/\\\\\\\\\"/g")\\\")\""
+    done
+) | netcat localhost 8080 | while read -r line; do
+    timestamp="$(echo "$line" | sed -E "s/\"text\(([0-9]+),.*/\1/g")"
+    sender="$(echo "$line" | sed -E "s/\"text\(([0-9]+),\\\\\"([^ ]+):.*/\2/g")"
+    raw_text="$(echo "$line" | sed -E "s/\"text\(([0-9]+),\\\\\"([^ ]+): (.*)\\\\\"\)\"/\3/g" | sed -E "s/\\\\//g")"
+    escaped_text="$(printf "%q" "$raw_text")"
+    display_text="$(eval "echo $escaped_text")"
+
+    echo "[$(date -d @"$timestamp")] $sender: $display_text"
+done
+
