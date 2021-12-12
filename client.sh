7345654eac64a7be0118506d0ea79484b46445f1
Reed Oei <oei.reed@gmail.com>
2021-12-12 10:38:06 -0800

    Add reply functionality

--- client.sh
+++ client.sh
@@ -9,11 +9,16 @@ read -r name
     while read -r line; do
         if [[ "$line" =~ ^\/all ]]; then
             echo "\"allMessages\""
+        elif [[ "$line" =~ ^\/re ]]; then
+            msg_id="$(echo "$line" | cut -f2 -d":")"
+            msg="$(echo "$line" | sed -E "s/^\/re[^:]*:[0-9]+:(.*)+/\1/g")"
+            echo "\"replyTo($msg_id, $(date +"%s"), \\\"$(echo "$msg" | sed -E "s/\"/\\\\\\\\\"/g")\\\")\""
         else
             echo "\"text($(date +"%s"), \\\"$(echo "$line" | sed -E "s/\"/\\\\\\\\\"/g")\\\")\""
         fi
     done
 ) | netcat localhost 8080 | while read -r line; do
+    echo "Raw: $line"
     timestamp="$(echo "$line" | sed -E "s/\"text\(([0-9]+),.*/\1/g")"
     sender="$(echo "$line" | sed -E "s/\"text\(([0-9]+),\\\\\"([^ ]+):.*/\2/g")"
     raw_text="$(echo "$line" | sed -E "s/\"text\(([0-9]+),\\\\\"([^ ]+): (.*)\\\\\"\)\"/\3/g" | sed -E "s/\\\\//g")"
